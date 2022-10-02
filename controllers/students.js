d446cd54400f87a9014ccc306ac135ac0f1520d2
Anesu shangwa <97034450+Anesushangwa@users.noreply.github.com>
2022-10-02 15:21:14 +0200

    new

--- controllers/students.js
+++ controllers/students.js
@@ -1,5 +1,6 @@
 const mongodb = require('../db/connection');
 const ObjectId = require('mongodb').ObjectId;
+// const {userDataValidate} = require("../validation/user.validation");
 
 const getAll = async (req, res, next) => {
   const result = await mongodb.getDb().db('student_profile').collection('students').find();
@@ -9,22 +10,22 @@ const getAll = async (req, res, next) => {
   });
 };
 
-// const getSingle = async (req, res, next) => {
-//   const userId = new ObjectId(req.params.id);
-//   const result = await mongodb
-//     .getDb()
-//     .db('anesu')
-//     .collection('contacts')
-//     .find({ _id: userId });
-//   result.toArray().then((lists) => {
-//     res.setHeader('Content-Type', 'application/json');
-//     res.status(200).json(lists[0]);
-//   });
-// };
+const getSingle = async (req, res, next) => {
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb
+    .getDb()
+    .db('student_profile')
+    .collection('students')
+    .find({ _id: userId });
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists[0]);
+  });
+};
 
 
 
-const newinfo = async (req, res) => {
+const newinfo = async (req,res, next) => {
   const newcontact = {
     
       username: req.body.username,
@@ -37,60 +38,68 @@ const newinfo = async (req, res) => {
       computer_scince:req.body.computer_scince,
       Maths:req.body.Maths,
       english:req.body.english,
-      Shona:req.body.shona,
+      shona:req.body.shona,
       wood_work:req.body.wood_work,
       date_of_birth:req.body.date_of_birth
   };
   const sendinfo = await mongodb.getDb().db('student_profile').collection('students').insertOne(newcontact);
 
-//   try {
+  try {
     
-//       res.status(201).json(sendinfo)
-//   }
-//   catch (error) {
-//       res.status(500).json({message: error.message})
-//   };
-// }
-if (sendinfo.acknowledged) {
-  res.status(201).json(sendinfo);
-} else {
-  res.status(500).json(sendinfo.error || 'Some error occurred while creating the contact.');
+      res.status(201).json(sendinfo)
+  }
+  catch (error) {
+      res.status(500).json({message: error.message})
+  };
 }
-};
+
+// if (sendinfo.acknowledged) {
+//   res.status(201).json(sendinfo);
+// } else {
+//   res.status(500).json(sendinfo.error || 'Some error occurred while creating the contact.');
+// }
+// };
 
 // const updateContact = async (req, res) => {
 // const userId = new ObjectId(req.params.id);
 // const newcontact= {
   
-//      username: req.body.username,
-//       firstname: req.body.firstname,
-//       email: req.body.email,
-//       email: req.body.email,
-      
-//       birthday: req.body.birthday
+//   username: req.body.username,
+//   firstname: req.body.firstname,
+//   lastname: req.body.lastname,
+//   email:req.body.email,
+//   home_adress:req.body.home_adress,
+//   number_subject_registered: req.body.number_subject_registered,
+//   about:req.body.about,
+//   computer_scince:req.body.computer_scince,
+//   Maths:req.body.Maths,
+//   english:req.body.english,
+//   Shona:req.body.shona,
+//   wood_work:req.body.wood_work,
+//   date_of_birth:req.body.date_of_birth
   
 // };
 
-// try {
-//   const dataToSave = await data.save();
-//   res.status(200).json(dataToSave)
-// }
-// catch (error) {
-//   res.status(400).json({message: error.message})
-// }
-// });
-// const sendinfo = await mongodb
-// .getDb()
-// .db('anesu')
-// .collection('contacts')
-// .replaceOne({ _id: userId }, newcontact);
-// console.log(sendinfo);
-// if ( sendinfo.modifiedCount > 0) {
-// res.status(204).send();
-// } else {
-// res.status(500).json(sendinfo.error || 'Some error occurred while updating the contact.');
-// }
-// };
+// // try {
+// //   const dataToSave = await data.save();
+// //   res.status(200).json(dataToSave)
+// // }
+// // catch (error) {
+// //   res.status(400).json({message: error.message})
+// // }
+// // });
+// // const sendinfo = await mongodb
+// // .getDb()
+// // .db('anesu')
+// // .collection('contacts')
+// // .replaceOne({ _id: userId }, newcontact);
+// // console.log(sendinfo);
+// // if ( sendinfo.modifiedCount > 0) {
+// // res.status(204).send();
+// // } else {
+// // res.status(500).json(sendinfo.error || 'Some error occurred while updating the contact.');
+// // }
+// // };
 
 // const deleteinfo = async (req, res) => {
 //   const userId = new ObjectId(req.params.id);
@@ -103,5 +112,55 @@ if (sendinfo.acknowledged) {
 //   }
 // };
 
+const updatestu = async (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid information id to update for a student.');
+  }
+  const userId = new ObjectId(req.params.id);
+  // be aware of updateOne if you only want to update specific fields
+  const contact = {
+    username: req.body.username,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    email:req.body.email,
+    home_adress:req.body.home_adress,
+    number_subject_registered: req.body.number_subject_registered,
+    about:req.body.about,
+    computer_scince:req.body.computer_scince,
+    Maths:req.body.Maths,
+    english:req.body.english,
+    shona:req.body.shona,
+    wood_work:req.body.wood_work,
+    date_of_birth:req.body.date_of_birth
+    
+  };
+  const response = await mongodb
+    .getDb()
+    .db('student_profile')
+    .collection('students')
+    .replaceOne({ _id: userId }, contact);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the students.');
+  }
+};
 
-  module.exports = { getAll, newinfo};
+const deleteinfo = async (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid student id to delete a student.');
+  }
+  const userId = new ObjectId(req.params.id);
+  const response = await mongodb.getDb().db('student_profile').collection('students').remove({ _id: userId }, true);
+  console.log(response);
+  if (response.deletedCount > 0) {
+    res.status(200).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while deleting the student.');
+  };
+}
+
+  module.exports = { getAll, newinfo,updatestu,deleteinfo,getSingle}
+
+
--- routes/students.js
+++ routes/students.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const router = express.Router();
-
+const  validation = require("../validation/user.validation");
 const contactsController = require('../controllers/students');
 
 router.get('/', contactsController.getAll);
 
-// router.get('/:id', contactsController.getSingle);
+router.get('/:id', contactsController.getSingle);
 
 //Post Method
-router.post('/', contactsController.newinfo)
+router.post('/',   validation.saveContact, contactsController.newinfo)
 
-// router.put('/:id', contactsController.updateContact);
+router.put('/:id', validation.saveContact, contactsController.updatestu );
 
-// router.delete('/:id', contactsController.deleteinfo);
+router.delete('/:id', contactsController.deleteinfo);
+
+// getAll, newinfo,updatestu,deleteinfo,getSingle
 
 
 module.exports = router;
